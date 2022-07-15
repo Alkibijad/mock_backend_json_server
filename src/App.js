707fe4fb37a7f23d92975f0d5991bcb4bcd1e3b0
Alkibijad <aleksandar04081@gmail.com>
2022-07-15 09:12:54 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,94 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from "react";
+import "./index.css"
+
 
 function App() {
+  const [feedback, setFeedback] = useState([]);
+  const [text, setText] = useState();
+  const [editID, setEditID] = useState(null)
+  const textArea = useRef()
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      "http://localhost:5000/feedback?_sort=id&_order=desc"
+    );
+    const data = await response.json();
+    setFeedback(data);
+  };  
+  const displayFeedback = () => {
+    return feedback.map((el, index) => {
+      return (
+        <div key={index} className="feedback_card">
+          <p>ID: {el.id}</p>
+          <p>Rate: {el.text}</p>
+          <p>Rating: {el.rating}</p>
+          <p onClick={() => { deleteRating(el.id) }}>X</p>
+          <button type="button" onClick={() => { updateRating( index) }}>edit</button>
+        </div>
+      );
+    });
+  }; 
+  const deleteRating = async (id) => { 
+    if (window.confirm("Are you sure you want to delete this review")) {
+      let deletedArray = feedback.filter((el) => el.id !== id)
+      setFeedback(deletedArray)
+      displayFeedback()
+      const response = await fetch(`http://localhost:5000/feedback/${id}`, {
+      method: "DELETE",
+    });
+    
+   }
+  }
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+  const addFeedback = async () => {
+    const newFeedback = {
+      text: text
+    };
+    const response = await fetch("http://localhost:5000/feedback", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newFeedback),
+    });
+    const data = await response.json();
+    setFeedback([data, ...feedback]);
+  };
+  const updateRating = async (index) => { 
+    textArea.current.value = feedback[index].text
+    setEditID(feedback[index].id)
+  }
+  const saveEdited =async () => { 
+    const response = await fetch(`http://localhost:5000/feedback/${editID}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ text: textArea.current.value }),
+    });
+    fetchFeedback()
+    setEditID(null)
+  }
+
   return (
-    <div>App</div>
-  )
+    <div>
+    
+      {feedback ? displayFeedback() : <p>Error 404</p>}
+      <form>
+        <textarea
+          ref={textArea}
+          name="addtext"
+          id=""
+          cols="30"
+          rows="10"
+          onChange={handleTextChange}
+        ></textarea>
+        { editID ? null : <button type="reset" onClick={addFeedback}>Submit</button>  }
+        { editID && <button type="reset" onClick={saveEdited}>Save Edit</button>}
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
